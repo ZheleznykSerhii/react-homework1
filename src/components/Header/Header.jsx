17979ea02fc62edaf24f1728ea8eb2baf9c99d1a
ZheleznykSerhii <83211761+ZheleznykSerhii@users.noreply.github.com>
2021-11-15 18:20:34 +0200

    fixed route on home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import HeaderText from './HeaderText'
 const Header = () => {
   return (
     <Routes>
-      <Route path="/login" element={<HeaderText text="Home Page" />} />
+      <Route path="/home" element={<HeaderText text="Home Page" />} />
       <Route path="/catalog" element={<HeaderText text="Catalog Page" />} />
       <Route path="/cart" element={<HeaderText text="Cart Page" />} />
     </Routes>
